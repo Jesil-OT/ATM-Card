81bfc3c72d52ec924386fd8951df1f739f833f6e
Jesil-OT <toboroweijesil@gmail.com>
2021-12-09 05:01:39 -1100

    done with xml layouts

--- app/src/main/java/com/jesil/card/ui/MainActivity.kt
+++ app/src/main/java/com/jesil/card/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.jesil.card.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.jesil.card.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.card_fragment_container_view) as NavHostFragment
+        navController = navHostFragment.findNavController()
     }
 }
